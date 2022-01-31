e36ef343c90f79ad71f18de558baad494d10ecb8
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2022-02-01 05:07:18 +0530

    Adding errors and middleware

---
+++ common/src/index.ts
@@ -0,0 +1,8 @@
+export * from "./errors/bad-request-error";
+export * from "./errors/base-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation";
+
+export * from "./middleware/current-user";
+export * from "./middleware/error-handler";
+export * from "./middleware/request-validation";
---
+++ common/src/middleware/current-user.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
+// import { User } from "../models/userModel";
+
+interface UserPayload {
+  id: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: UserPayload | null;
+    }
+  }
+}
+
+export const currentUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  if (!token) {
+    throw new BadRequestError("You are not logged in! Please login first.");
+  }
+  const decode = jwt.verify(token, process.env.JWT_KEY!) as UserPayload;
+
+  // const currentUser = await User.findById(decode.id);
+
+  req.user = decode;
+
+  next();
+};
