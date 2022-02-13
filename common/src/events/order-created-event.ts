b9ffa3ec5218e97fd2edcfd25bf0bf56bc262a07
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2022-02-13 12:18:06 +0530

    Updates

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -2,7 +2,7 @@ import { Subjects } from "./subjects";
 import { OrderEventStatus } from "./types/order-event-status";
 
 export interface OrderCreatedEvent {
-  subjects: Subjects.OrderCreated;
+  subject: Subjects.OrderCreated;
   data: {
     id: string;
     status: OrderEventStatus;
